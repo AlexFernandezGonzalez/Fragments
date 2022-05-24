d2d978cc9cbc25fc936191474a580fcd667afbed
Alex <afernandezgonzalez@danielcastelao.org>
2022-05-24 10:57:30 +0200

    Añadimos proyecto

---
+++ app/src/main/java/com/alex/fragments2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.alex.fragments2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import android.app.Fragment
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
